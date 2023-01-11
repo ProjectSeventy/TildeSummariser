7a177523075cb439d6e4c7e393533394e134fdff
ProjectSeventy <g.chandler@lancaster.ac.uk>
2023-01-11 13:11:52 +0000

    refactor: Genericised dynamic curve generation.

--- TildeSummariser/TildeSummariser.py
+++ TildeSummariser/TildeSummariser.py
@@ -14,11 +14,18 @@ class TildeSummariser():
         )
     ):
         self.curve_coefficients = curve_coefficients
-        slef.nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def _get_segment_length_from_curve(self, midpoint_percentage):
+        num_sentences = 0
+        for i in range(len(self.curve_coefficients), 0, step=-1):
+            num_sentences += pow(midpoint_percentage, i)*self.curve_coefficients[i]
+        num_sentences = round(num_sentences)
+        return num_sentences
 
     def summarise(self, content, n_segment, n_total, dynamic_flag = False):
         #pre-process
-        content = list(nlp.pipe(content))
+        content = list(self.nlp.pipe(content))
         content = [doc for doc in content if not (len(doc) == 1 and doc[0].text == '\n')]
         
         #Segment
@@ -36,7 +43,7 @@ class TildeSummariser():
                 #Get the midpoint of the segment as a percentage of the full text
                 perc = ((last_sent - (len(segments_sented[i])/2))/len_all_seg)*100
                 #Calculate the length according to the curve
-                n_segment = round((x*(perc*perc*perc))+(y*(perc*perc))+(z*perc)+c)
+                n_segment = self._get_segment_length_from_curve(perc)
             #Get the summary sentences for the segment
             all_summary_sentences += get_summary_sentences(segments[i], segments_sented[i], n_segment)
         #Convert all summaries to one doc and generate a summary of them
